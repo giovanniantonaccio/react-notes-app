8f413990f82cae7dd0d30a4f0db836ce0e121dbe
Giovanni Bessa <giovanni.bessa@conversica.com>
2022-01-19 01:31:01 -0400

    Add Note and SideMenu

--- components/Note.jsx
+++ components/Note.jsx
@@ -1,15 +1,33 @@
-import { Button, Card, Tooltip } from "antd";
+import { Button, Card, Col, Popover, Tooltip } from "antd";
 import React from "react";
-import { MoreOutlined } from "@ant-design/icons";
+import { MoreOutlined, DeleteOutlined, EditOutlined } from "@ant-design/icons";
+import styles from "../styles/Note.module.css";
 
 export default function Note() {
+  const content = (
+    <Col>
+      <Button type="text" style={{ width: "100%" }} icon={<EditOutlined />}>
+        Edit
+      </Button>
+      <Button
+        type="text"
+        style={{ width: "100%", color: "red" }}
+        icon={<DeleteOutlined />}
+      >
+        Delete
+      </Button>
+    </Col>
+  );
+
   return (
     <Card
       title="Default size card"
       extra={
-        <Tooltip title="Menu">
-          <Button type="text" shape="circle" icon={<MoreOutlined />} />
-        </Tooltip>
+        <Popover placement="bottomRight" content={content}>
+          <Tooltip title="Menu">
+            <Button type="text" shape="circle" icon={<MoreOutlined />} />
+          </Tooltip>
+        </Popover>
       }
       style={{ width: "100%" }}
     >
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,15 @@
-import styles from "../styles/Home.module.css";
+import { useState } from "react";
+import { Button, Layout } from "antd";
 
-import { Col, Layout, Menu, Row } from "antd";
-import {
-  AppstoreOutlined,
-  BarChartOutlined,
-  CloudOutlined,
-  ShopOutlined,
-  TeamOutlined,
-  UserOutlined,
-  UploadOutlined,
-  VideoCameraOutlined,
-} from "@ant-design/icons";
 import NotesMenuHeader from "../components/NotesMenuHeader";
 import NotesMenuBody from "../components/NotesMenuBody";
+import SideMenu from "../components/SideMenu";
+import styles from "../styles/Home.module.css";
 const { Header, Content, Footer, Sider } = Layout;
 
 export default function Home() {
+  const [isSideMenuCollapsed, setIsSideMenuCollapsed] = useState(false);
+
   return (
     <Layout hasSider>
       <Sider
@@ -27,40 +21,23 @@ export default function Home() {
           top: 0,
           bottom: 0,
         }}
+        collapsible
+        collapsed={isSideMenuCollapsed}
       >
-        <div className={styles.logo} />
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={["4"]}>
-          <Menu.Item key="1" icon={<UserOutlined />}>
-            nav 1
-          </Menu.Item>
-          <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-            nav 2
-          </Menu.Item>
-          <Menu.Item key="3" icon={<UploadOutlined />}>
-            nav 3
-          </Menu.Item>
-          <Menu.Item key="4" icon={<BarChartOutlined />}>
-            nav 4
-          </Menu.Item>
-          <Menu.Item key="5" icon={<CloudOutlined />}>
-            nav 5
-          </Menu.Item>
-          <Menu.Item key="6" icon={<AppstoreOutlined />}>
-            nav 6
-          </Menu.Item>
-          <Menu.Item key="7" icon={<TeamOutlined />}>
-            nav 7
-          </Menu.Item>
-          <Menu.Item key="8" icon={<ShopOutlined />}>
-            nav 8
-          </Menu.Item>
-        </Menu>
+        <SideMenu />
       </Sider>
       <Layout className={styles["site-layout"]} style={{ marginLeft: 200 }}>
         <Header
           className={styles["site-layout-background"]}
           style={{ padding: 0 }}
-        />
+        >
+          <Button
+            type="primary"
+            onClick={() => setIsSideMenuCollapsed(!isSideMenuCollapsed)}
+          >
+            Menu
+          </Button>
+        </Header>
         <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
           <div
             className="site-layout-background"
